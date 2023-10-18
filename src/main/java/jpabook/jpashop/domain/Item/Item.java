dfa9167a9fdf9f3128e870849c715a62cdf703a5
한창윤 <hansyooni11@gmail.com>
2023-10-18 18:28:03 +0900

    domain, web layer structure
    member, item, order domain, web layer structure(model, view, controller)

--- src/main/java/jpabook/jpashop/domain/Item/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.Item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -38,5 +39,6 @@ public abstract class Item {
         if(restStock < 0){
             throw new NotEnoughStockException("need more stock");
         }
+        this.stockQuantity = restStock;
     }
 }
